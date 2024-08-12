39f173b9aaeee85865454186128374653cfb43fe
Vinicius Arai <kansaiarai@gmail.com>
2024-08-12 20:07:09 -0300

    exercicios1

---
+++ exercicio1.py
@@ -0,0 +1,12 @@
+#UMA EMPRESA DE DESENVOLVIMENTO DE SOFTWARES PAGA A SEU VENDEDOR UM FIXO DE R$1500 POR MÊS, MAIS UM BÔNUS DE R$50 POR SOFTWARE VENDIDO. FAÇA UM ALGORITMO QUE LEIA A QUANTIDADE DE SOFTWARES VENDIDOS E CALCULE O SALÁRIO TOTAL DO FUNCIONÁRIO
+
+# Entrada de dados
+quantidade_softwares = int(input("Digite a quantidade de softwares vendidos: "))
+
+# Cálculo do salário total
+salario_fixo = 1500
+bonus_por_software = 50
+salario_total = salario_fixo + (quantidade_softwares * bonus_por_software)
+
+# Saída de dados
+print("Salário total: R$", salario_total)
